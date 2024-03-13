8bc2f2eba318fb081d005dd67592aaeec175ad0b
Fabio D. Junior <fabiojr@skiff.com>
2024-03-13 01:58:08 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,40 +3,41 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Endpoint de Login
 @app.route('/auth/login', methods=['POST'])
 def login():
     username = request.headers.get('username')
     secret = request.headers.get('secret')
     if username and secret:
-        return jsonify(token="mocked_token")
+        return jsonify(token='eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I')
     else:
-        return jsonify(message="credentials_missing"), 400
+        return {"message": "Wrong Credentials. Try Again"}, 400
 
+# Endpoint de Simulação
 @app.route('/bot/simulate', methods=['GET'])
 def simulate():
     auth_header = request.headers.get('Authorization')
     token = auth_header.split(" ")[1] if auth_header and auth_header.startswith("Bearer ") else None
-    if not token or token != "mocked_token":
-        return jsonify(message="invalid_or_missing_token"), 401
+    if not token or token != 'eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I':
+        return {"message": "Invalid or missing token"}, 401
     
-    # Mocked data for simulation response
+    assets = pd.read_csv("save_action_memory.csv")
     data = {
-        'result': ['simulation_result'],
-        'details': ['Simulation details here'],
+        'asset_history': assets.to_json(),
         'current_balance': 10000,
         'asset_value': 15000,
         'sharpe_ratio': 1.25
     }
-    df = pd.DataFrame([data])
     
-    return jsonify(df.to_dict(orient="records")[0])
+    return jsonify(data)
 
+# Endpoint do Chatbot
 @app.route('/chatbot/ask', methods=['GET'])
 def ask():
     auth_header = request.headers.get('Authorization')
     token = auth_header.split(" ")[1] if auth_header and auth_header.startswith("Bearer ") else None
-    if not token or token != "mocked_token":
-        return jsonify(message="invalid_or_missing_token"), 401
+    if not token or token != 'eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I':
+        return {"message": "Invalid or missing token"}, 401
     
     user_message = request.args.get('user_message')
-    return jsonify(bot_message=f"Mocked response to '{user_message}'")
+    return jsonify({'bot_message': f"Mocked response to '{user_message}'"})
